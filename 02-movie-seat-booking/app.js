a931d111bf98304cb8a6de383ac1e2ce2fa5ff24
Moshe Gelberg <glbrgm@gmail.com>
2020-02-05 11:42:58 +0200

    2. Movie Seat Booking

---
+++ 02-movie-seat-booking/app.js
@@ -0,0 +1,61 @@
+/** @type {HTMLDivElement} */
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+
+/** @type {HTMLSelectElement} */
+const movieSelect = document.getElementById('movie-select');
+let ticketPrice = +movieSelect.value;
+
+
+container.addEventListener('click', event => {
+  if (event.target.classList.contains('seat') &&
+      !event.target.classList.contains('occupied')) {
+    event.target.classList.toggle('selected');
+    updateInfo();
+    setLocalStorageData();
+  }
+});
+
+movieSelect.addEventListener('change', () => {
+  ticketPrice = +movieSelect.value;
+  updateInfo();
+  setLocalStorageData();
+});
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selected-seats-indexes'));
+  if (selectedSeats && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+  const selectedMovieIdx = +localStorage.getItem('selected-movie-index');
+  selectedMovieIdx && (movieSelect.selectedIndex = selectedMovieIdx);
+  ticketPrice = +localStorage.getItem('selected-movie-price');
+  updateInfo();
+}
+
+function updateInfo() {
+  count.textContent = getSelectedSeatsCount().toString();
+  total.textContent = getSelectedSeatsPrice().toString();
+}
+
+function setLocalStorageData() {
+  localStorage.setItem('selected-seats-indexes', JSON.stringify(getSelectedSeatsIndexes()));
+  localStorage.setItem('selected-movie-index', movieSelect.selectedIndex.toString());
+  localStorage.setItem('selected-movie-price', movieSelect.value);
+}
+
+const getSelectedSeatsCount = () => document.querySelectorAll('.row .seat.selected').length;
+const getSelectedSeatsPrice = () => getSelectedSeatsCount() * ticketPrice;
+
+const getSelectedSeatsIndexes = () => {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  return [...selectedSeats].map(seat => [...seats].indexOf(seat));
+};
+
+populateUI();
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # 20 JavaScript Projects
 
-[1. Form Validator](https://kosemmg.github.io/20-js-projects/01-form-validator/ "Form Validator")
+1. [Form Validator](https://kosemmg.github.io/20-js-projects/01-form-validator/ "Form Validator")
+2. [Movie Seat Booking](https://kosemmg.github.io/20-js-projects/02-movie-seat-booking/ "Movie Seat Booking App") (DOM & Local Storage)
 
